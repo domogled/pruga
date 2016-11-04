0927163a17faaf83d3727c1973cd63a25a816cc5
Петр Болф <petr.bolf@domogled.com>
2016-11-04 23:44:38 +0100

    ještě hrátky z docopt, ale přímé parsování argumentu do Path zlobilo

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,8 @@ Options:
 
 If no arguments are provided, then 'пруга' will run `build` and `test`
 ",
-arg_part: Option<std::string::String>);
+arg_part: Option<std::string::String>
+/*, flag_project_directory: PathBuf*/);
 
 fn main() {
 
@@ -54,9 +55,7 @@ fn main() {
         }
     }*/
 
-    let cwd = Path::new(&args.flag_project_directory).canonicalize().unwrap();
-    
-    //is_workspace(&args.flag_project_directory).unwrap();
+    let cwd = /*args.flag_project_directory;*/ Path::new(&args.flag_project_directory).canonicalize().unwrap(); //is_workspace(&args.flag_project_directory).unwrap();
     
     let pruga_root_path = find_project_root_directory(&cwd, "pruga.toml")
         .expect("Adresář {:?} není adresářem projektu Пруга");
@@ -84,6 +83,7 @@ fn parse_options() {
     assert!(args.cmd_to);
     assert_eq!(args.arg_destination, vec![s("dest/")]);
     assert_eq!(args.flag_project_directory, s("./"));
+    // assert_eq!(args.flag_project_directory, PathBuf::from("./"));
     // assert_eq!(args.arg_dest, s(""));
 }
 
@@ -92,7 +92,7 @@ fn parse_options_2() {
     
     fn s(x: &str) -> String { x.to_string() }
 
-    let argv = || vec!["pruga", "from", "view", "to", "dest/", "--project-directory", "./home/rust/pruga"];
+    let argv = || vec!["pruga", "from", "view", "to", "dest/", "--project-directory", "/home/rust/pruga"];
 
     let args: Args = Args::docopt().argv(argv().into_iter()).decode().unwrap_or_else(|e| e.exit());
 
@@ -101,6 +101,7 @@ fn parse_options_2() {
     assert_eq!(args.arg_part, Some(s("view")));
     assert!(args.cmd_to);
     assert_eq!(args.arg_destination, vec![s("dest/")]);
-    assert_eq!(args.flag_project_directory, s("./home/rust/pruga"));
+    assert_eq!(args.flag_project_directory, s("/home/rust/pruga"));
+    // assert_eq!(args.flag_project_directory, PathBuf::from("/home/rust/pruga"));
     // assert_eq!(args.arg_dest, s(""));
 }
