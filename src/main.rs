d99c450b6ff14fdea71f1409d505267a5c308456
Петр Болф <petr.bolf@domogled.com>
2016-11-04 23:31:39 +0100

    výchozí adresář lépe a radostněji

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ extern crate rustc_serialize;
 extern crate docopt;
 
 use util::important_paths::{find_project_root_directory};
-use std::env;
-use std::path::PathBuf;
-// use std::path::Path;
+// use std::env;
+// use std::path::PathBuf;
+use std::path::Path;
 
 
 mod build;
@@ -21,30 +21,21 @@ mod util;
 
 docopt!(Args derive Debug, "
 Usage:
-    pruga from <part>... to <destination>... [--project-directory=<project_directory>]
+    pruga from <part> to <destination>... [--project-directory=<project_directory>]
     pruga set <part> to <value> [--project-directory=<project_directory>]
 
 Options:
   -h, --help    Display this message
   --version     Show version.
-  --project-directory=<project_directory>     set root project directory
+  --project-directory=<project_directory>     set root project directory [default: ./]
 
 `pruga filename` compile fielname to web application
 
 If no arguments are provided, then 'пруга' will run `build` and `test`
-");
+",
+arg_part: Option<std::string::String>);
 
 fn main() {
-
-    // let argv = || vec!["pruga", "pruga.toml"];
-
-    // println!("Hello, Пруга!");
-    
-    // let current_line = line!();
-    // let this_file = file!();
-    // println!("defined in file: {} on line: {}", this_file, current_line);
-
-    // println!("log level = {:?}", log::max_log_level());
 
     // Initialize logger
     env_logger::init().unwrap();
@@ -55,15 +46,17 @@ fn main() {
 
     println!("argumenty {:?}", args);
     
-    // @TODO better default value from command line
-    fn is_workspace(working_path: &str) -> Result<PathBuf, std::io::Error> {
+    // @TODO what is better default value a creating Path from command line
+    /*fn is_workspace(working_path: &str) -> Result<PathBuf, std::io::Error> {
         match working_path {
             "" => env::current_dir(), //.unwrap().as_path()
             path => Ok(PathBuf::from(&path)),  
         }
-    }
+    }*/
 
-    let cwd = is_workspace(&args.flag_project_directory).unwrap();
+    let cwd = Path::new(&args.flag_project_directory).canonicalize().unwrap();
+    
+    //is_workspace(&args.flag_project_directory).unwrap();
     
     let pruga_root_path = find_project_root_directory(&cwd, "pruga.toml")
         .expect("Adresář {:?} není adresářem projektu Пруга");
@@ -81,16 +74,16 @@ fn parse_options() {
     
     fn s(x: &str) -> String { x.to_string() }
 
-    let argv = || vec!["pruga", "from", "file1", "file2", "to", "dest/"];
+    let argv = || vec!["pruga", "from", "view", "to", "dest/"];
 
     let args: Args = Args::docopt().argv(argv().into_iter()).decode().unwrap_or_else(|e| e.exit());
 
     // Now access your argv values.
     assert!(args.cmd_from);
-    assert_eq!(args.arg_part, vec![s("file1"), s("file2")]);
+    assert_eq!(args.arg_part, Some(s("view")));
     assert!(args.cmd_to);
     assert_eq!(args.arg_destination, vec![s("dest/")]);
-    assert_eq!(args.flag_project_directory, s(""));
+    assert_eq!(args.flag_project_directory, s("./"));
     // assert_eq!(args.arg_dest, s(""));
 }
 
@@ -99,13 +92,13 @@ fn parse_options_2() {
     
     fn s(x: &str) -> String { x.to_string() }
 
-    let argv = || vec!["pruga", "from", "file1", "file2", "to", "dest/", "--project-directory", "./home/rust/pruga"];
+    let argv = || vec!["pruga", "from", "view", "to", "dest/", "--project-directory", "./home/rust/pruga"];
 
     let args: Args = Args::docopt().argv(argv().into_iter()).decode().unwrap_or_else(|e| e.exit());
 
     // Now access your argv values.
     assert!(args.cmd_from);
-    assert_eq!(args.arg_part, vec![s("file1"), s("file2")]);
+    assert_eq!(args.arg_part, Some(s("view")));
     assert!(args.cmd_to);
     assert_eq!(args.arg_destination, vec![s("dest/")]);
     assert_eq!(args.flag_project_directory, s("./home/rust/pruga"));
